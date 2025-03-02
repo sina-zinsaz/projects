1ba6e1bf9d9178f4a37c760f9378ebaffc657dc2
Sina Zinsaz <101419921+sina-zinsaz@users.noreply.github.com>
2025-03-02 12:07:20 +0330

    Update server.js

--- blogify/server.js
+++ blogify/server.js
@@ -1,21 +1,3 @@
-// const express = require('express');
-// const path = require('path');
-// const engine = require('ejs-mate');
-
-// const app = express();
-
-// app.engine('ejs', engine); // تنظیم ejs-mate به عنوان موتور قالب
-// app.set('view engine', 'ejs');
-// app.set('views', path.join(__dirname, 'views'));
-
-// app.get('/', (req, res) => {
-//     res.render('index', { title: 'صفحه اصلی' });
-// });
-
-// app.listen(3000, () => {
-//     console.log('Server is running on port 3000');
-// });
-
 const express = require('express');
 const path = require('path');
 
@@ -24,17 +6,14 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-// صفحه اصلی
 app.get('/', (req, res) => {
     res.render('layout', { body: 'index' });
 });
 
-// صفحه درباره ما
 app.get('/about', (req, res) => {
     res.render('layout', { body: 'about' });
 });
 
-// صفحه پست‌ها
 app.get('/post/:id', (req, res) => {
     const posts = [
         { id: 1, title: "پست اول", content: "محتوای پست اول" },
